cb84d931e7e9496812fcd339dbfe09683553b785
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-01-18 13:44:55 -0300

    First App

---
+++ FirstApp/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    /*
+    This is the first comment line
+    This is the second comment line
+     */
+
+    public static void main(String[] args) {
+	    System.out.println("First line from app"); // This is a line comment
+        System.out.println("Second line from app");
+        System.out.println("Third line from app");
+    }
+}
