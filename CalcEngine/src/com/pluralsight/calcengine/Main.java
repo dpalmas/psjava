c9d805bb8f820613050d2427e4df11c212187fae
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-01-19 20:56:56 -0300

    Using Block Statements

--- CalcEngine/src/com/pluralsight/calcengine/Main.java
+++ CalcEngine/src/com/pluralsight/calcengine/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	    double value1 = 100.0d;
 	    double value2 = 50.0d;
 	    double result = 0.0d;
-	    char opCode = 'a';
+	    char opCode = 'd';
 
 	    if (opCode == 'a')
 	        result = value1 + value2;
@@ -14,11 +14,14 @@ public class Main {
 	        result = value1 - value2;
 	    else if (opCode == 'm')
 	        result = value1 * value2;
-	    else if (opCode == 'd')
-	        result = value1 / value2;
-	    else
-	        result = 0.0d;
-
+	    else if (opCode == 'd') {
+	    	if (value2 != 0)
+				result = value1 / value2;
+		}
+	    else {
+	    	System.out.println("Invalid opCode: " + opCode);
+			result = 0.0d;
+		}
 	    System.out.println(result);
     }
 }
