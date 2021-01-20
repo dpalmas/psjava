83b7be5333d62ea906f1ba38132c3224e1e6c8f1
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-01-20 13:42:57 -0300

    Using arrays

--- CalcEngine/src/com/pluralsight/calcengine/Main.java
+++ CalcEngine/src/com/pluralsight/calcengine/Main.java
@@ -3,28 +3,30 @@ package com.pluralsight.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-	    double value1 = 100.0d;
-	    double value2 = 50.0d;
-	    double result = 0.0d;
-	    char opCode = 'd';
+        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+        char[] opCodes = {'a', 'd', 'm', 's'};
+        double[] results = new double[opCodes.length];
 
-	    switch (opCode) {
-			case 'a':
-	        	result = value1 + value2;
-	        	break;
-			case 's':
-				result = value1 - value2;
-				break;
-			case 'm':
-	        	result = value1 * value2;
-	        	break;
-			case 'd':
-				result = value2 != 0 ? value1 / value2 : 0.0d;
-				break;
-			default:
-	    		System.out.println("Invalid opCode: " + opCode);
-				result = 0.0d;
-		}
-	    System.out.println(result);
+        for (int i = 0; i < opCodes.length; i++) {
+            switch (opCodes[i]) {
+                case 'a':
+                    results[i] = leftVals[i] + rightVals[i];
+                    break;
+                case 's':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+                case 'm':
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                case 'd':
+                    results[i] = rightVals[i] != 0 ? leftVals[i] / rightVals[i] : 0.0d;
+                    break;
+                default:
+                    System.out.println("Invalid opCode: " + opCodes[i]);
+                    results[i] = 0.0d;
+            }
+            System.out.println(result);
+        }
     }
 }
