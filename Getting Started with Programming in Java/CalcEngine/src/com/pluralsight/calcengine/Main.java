31302fbfd9339881330b0f37ceef981f4341c6ae
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-01-21 11:39:20 -0300

    Update simple project

--- Getting Started with Programming in Java/CalcEngine/src/com/pluralsight/calcengine/Main.java
+++ Getting Started with Programming in Java/CalcEngine/src/com/pluralsight/calcengine/Main.java
@@ -8,13 +8,25 @@ public class Main {
         char[] opCodes = {'a', 's', 'm', 'd'};
         double[] results = new double[opCodes.length];
 
-        for (int i = 0; i < opCodes.length; i++) {
-            results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
-        }
+        if (args.length == 0) {
+            for (int i = 0; i < opCodes.length; i++) {
+                results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
+            }
 
-        for(double currentResult : results)
-            System.out.println(currentResult);
+            for(double currentResult : results)
+                System.out.println(currentResult);
+        } else if (args.length == 3)
+            handleCommandLine(args);
+        else
+            System.out.println("Please provide an operation code and 2 numeric values");
+    }
 
+    private static void handleCommandLine(String[] args) {
+        char opCode = args[0].charAt(0);
+        double leftVal = Double.parseDouble(args[1]);
+        double rightVal = Double.parseDouble(args[2]);
+        double result = execute(opCode, leftVal, rightVal);
+        System.out.println(result);
     }
 
     static double execute(char opCode, double leftVal, double rightVal) {
