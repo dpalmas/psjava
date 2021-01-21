24307bc865293e9afd17abc1444ba70672ac13f1
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-01-21 11:13:42 -0300

    Update simple project

--- Getting Started with Programming in Java/CalcEngine/src/com/pluralsight/calcengine/Main.java
+++ Getting Started with Programming in Java/CalcEngine/src/com/pluralsight/calcengine/Main.java
@@ -9,26 +9,34 @@ public class Main {
         double[] results = new double[opCodes.length];
 
         for (int i = 0; i < opCodes.length; i++) {
-            switch (opCodes[i]) {
-                case 'a':
-                    results[i] = leftVals[i] + rightVals[i];
-                    break;
-                case 's':
-                    results[i] = leftVals[i] - rightVals[i];
-                    break;
-                case 'm':
-                    results[i] = leftVals[i] * rightVals[i];
-                    break;
-                case 'd':
-                    results[i] = rightVals[i] != 0 ? leftVals[i] / rightVals[i] : 0.0d;
-                    break;
-                default:
-                    System.out.println("Invalid opCode: " + opCodes[i]);
-                    results[i] = 0.0d;
-            }
+            results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
         }
 
         for(double currentResult : results)
             System.out.println(currentResult);
+
+    }
+
+    static double execute(char opCode, double leftVal, double rightVal) {
+        double result;
+        switch (opCode) {
+            case 'a':
+                result = leftVal + rightVal;
+                break;
+            case 's':
+                result = leftVal - rightVal;
+                break;
+            case 'm':
+                result = leftVal * rightVal;
+                break;
+            case 'd':
+                result = rightVal != 0 ? leftVal / rightVal : 0.0d;
+                break;
+            default:
+                System.out.println("Invalid opCode: " + opCode);
+                result = 0.0d;
+                break;
+        }
+        return result;
     }
 }
