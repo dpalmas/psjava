ff1b4deffd2ba8649d77d74667b0e51c0530f645
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-01-21 10:36:38 -0300

    Basic exercises

--- Getting Started with Programming in Java/Exercises/ExitingMethod/src/com/pluralsight/exitingmethod/Main.java
+++ Getting Started with Programming in Java/Exercises/ExitingMethod/src/com/pluralsight/exitingmethod/Main.java
@@ -8,6 +8,8 @@ public class Main {
     }
 
     static void showSum(float x, float y, int count) {
+        if (count < 1)
+            return;
         float sum = x + y;
         for (int i = 0; i < count; i++) {
             System.out.println(sum);
