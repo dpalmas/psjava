f5eec7b5bb16fc91bb2ca812febec10271779e96
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-01-20 22:27:09 -0300

    Using if-else

---
+++ IfElse/src/com/pluralsight/ifelse/Main.java
@@ -0,0 +1,14 @@
+package com.pluralsight.ifelse;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int value1 = 10;
+        int value2 = 4;
+
+        if (value1 > value2)
+            System.out.println("value 1 is bigger");
+        else
+            System.out.println("value 1 is not bigger");
+    }
+}
