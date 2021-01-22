2bbd6bc3296b6e91d1c6d520b4fde2ea15782cc6
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-01-21 22:00:00 -0300

    Basic exercises

---
+++ Getting Started with Programming in Java/Exercises/StringBuilder/src/com/pluralsight/stringbuilder/Main.java
@@ -0,0 +1,21 @@
+package com.pluralsight.stringbuilder;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String location = "Tokyo";
+	    int flightNumber = 175;
+	    StringBuilder sb = new StringBuilder(40);
+	    sb.append("I flew to ");
+	    sb.append(location);
+	    sb.append(" on Flight #");
+	    sb.append(flightNumber);
+	    String message = sb.toString();
+	    String time = "9:00";
+	    int pos = sb.indexOf(" on");
+	    sb.insert(pos, " at ");
+	    sb.insert(pos + 4, time);
+	    message = sb.toString();
+	    System.out.println(message);
+    }
+}
