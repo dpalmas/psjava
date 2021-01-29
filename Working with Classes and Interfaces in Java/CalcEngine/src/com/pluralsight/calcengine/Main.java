c175b270dba3502f421140b4f453798e2edc8476
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-01-29 20:24:33 -0300

    Project with Classses and Interfaces

--- Working with Classes and Interfaces in Java/CalcEngine/src/com/pluralsight/calcengine/Main.java
+++ Working with Classes and Interfaces in Java/CalcEngine/src/com/pluralsight/calcengine/Main.java
@@ -7,22 +7,15 @@ public class Main {
     }
 
     private static void performCalculations() {
-        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
-        char[] opCodes = {'a', 's', 'm', 'd'};
-        double[] results = new double[opCodes.length];
-
         MathEquation[] equations = new MathEquation[4];
         equations[0] = create(100.0d, 50.0d, 'd');
         equations[1] = create(25.0d, 92.0d, 'a');
         equations[2] = create(225.0d, 17.0d, 's');
         equations[3] = create(11.0d, 3.0d, 'm');
 
-        for (int i = 0; i < opCodes.length; i++) {
-            results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
-        }
-        for (double currentResult : results) {
-            System.out.println("result = " + currentResult);
+        for (MathEquation equation: equations) {
+            equation.execute();
+            System.out.println("result = " + equation.result);
         }
     }
 
